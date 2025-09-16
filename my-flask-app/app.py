54dfb645bd841b8173be029075e2bb2ec63b073d
Wake <waiky.jung@gmail.com>
2025-09-16 21:05:36 +0000

    Working

--- my-flask-app/app.py
+++ my-flask-app/app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/random")
+def random_number():
+    number = random.randint(1, 100)
+    return render_template("random.html", random_number=number)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
