83feab3e9ad3a0cdfdb9d44b935b0de6b5980ac9
Wake <waiky.jung@gmail.com>
2025-09-17 16:21:58 +0000

    Test

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import requests
 import random
 
 app = Flask(__name__)
@@ -14,8 +15,17 @@ def random_number():
 
 @app.route("/ip_address")
 def ip_address():
-    display_ip_address = request.headers.get('X-Forwarded-For', request.remote_addr)
-    return render_template("ip.html", ip_address=display_ip_address)
+    visitor_ip_address = request.headers.get('X-Forwarded-For', request.remote_addr)
+    response = requests.get(f"https://ipapi.co/{visitor_ip_address}/json/").json()
+    if response.get("vpn") or response.get("proxy"):
+        is_vpn_or_proxy = True
+    else:
+        is_vpn_or_proxy = False
+
+    return render_template("ip.html", 
+        ip_address=visitor_ip_address,
+        vpn_status=is_vpn_or_proxy
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
