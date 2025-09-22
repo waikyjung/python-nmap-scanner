1c4fc945681fbcabd227699f64d0b2218cb14f06
Wake <waiky.jung@gmail.com>
2025-09-22 21:20:24 +0000

    Working

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import requests
+import nmap
 import random
+import re
 
 app = Flask(__name__)
 
@@ -21,13 +23,42 @@ def ip_address():
 @app.route("/check_ip", methods=["POST"])
 def check_ip():
     visitor_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    
+    #IP Info
     entered_ip = request.form.get("ip")
     response = requests.get(f"https://ipapi.co/{entered_ip}/json/").json()
-    print(response)
+    
+    #Nmap Scan
+    vulnerable_ports = {
+        21: 'FTP',
+        22: 'SSH',
+        23: 'Telnet',
+        25: 'SMTP',
+        53: 'DNS',
+        80: 'HTTP',
+        110: 'POP3',
+        139: 'SMB',
+        143: 'IMAP',
+        445: 'SMB',
+        3389: 'RDP',
+        5900: 'VNC'
+        }
+    ports_to_scan = ','.join(str(port) for port in list(vulnerable_ports.keys()))
+    nm = nmap.PortScanner()
+    nm.scan(entered_ip, ports_to_scan)
+
+    ports_status = {}
+    for host in nm.all_hosts():
+        for proto in nm[host].all_protocols():
+            for port in nm[host][proto]:
+                state = nm[host][proto][port]['state'].capitalize()
+                ports_status[f"{vulnerable_ports[port]} ({port})"] = state
+
     return render_template("ip.html", 
         ip_address=visitor_ip,
         checked_ip=entered_ip,
-        ip_response=response
+        ip_response=response,
+        nmap_response=ports_status
     )
 
 if __name__ == "__main__":
